b21e869e108278e8ca4b2a551f6cedb8331b2ff5
LeeJaeheee <sns3434@gmail.com>
2024-04-21 12:47:34 +0900

    [Feat] BaseView, BaseViewController 구현

---
+++ DogRun/Util/Base/BaseView.swift
@@ -0,0 +1,32 @@
+//
+//  BaseView.swift
+//  DogRun
+//
+//  Created by 이재희 on 4/20/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+        configureData()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+    func configureData() { }
+    
+}
+
+
---
+++ DogRun/Util/Base/BaseViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BaseViewController.swift
+//  DogRun
+//
+//  Created by 이재희 on 4/20/24.
+//
+
+import UIKit
+import RxSwift
+import Toast
+
+class BaseViewController<T: BaseView>: UIViewController {
+    
+    let mainView: T = T()
+    let disposeBag = DisposeBag()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        bind()
+        configureNavigation()
+    }
+    
+    func bind() { }
+    func configureNavigation() { }
+    
+    func showToast(_ message: String) {
+        self.view.hideAllToasts()
+        self.view.makeToast(message)
+    }
+
+}
