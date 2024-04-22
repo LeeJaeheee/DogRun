3d743cccbfe972f28034792532e0f4c951e937d6
LeeJaeheee <sns3434@gmail.com>
2024-04-23 01:22:23 +0900

    [Feat] Kingfisher URLRequest 요청 헤더 추가

--- DogRun/AppDelegate.swift
+++ DogRun/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import Kingfisher
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         IQKeyboardManager.shared.enable = true
+        
+        let modifier = AnyModifier { request in
+            var req = request
+            req.addValue(APIKey.sesacKey.rawValue, forHTTPHeaderField: HeaderLiteral.sesacKey.rawValue)
+            req.addValue(UserDefaultsManager.accessToken, forHTTPHeaderField: HeaderLiteral.authorization.rawValue)
+            return req
+        }
+        KingfisherManager.shared.defaultOptions = [.requestModifier(modifier)]
 
         return true
     }
